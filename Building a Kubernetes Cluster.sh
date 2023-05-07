df63757616d76de694685a4fc508d8e50cec22a4
demo1289 <demostration1289@proto.me>
2023-05-07 19:42:17 +0800

    Calico installation before cluster status chk

--- Building a Kubernetes Cluster.sh
+++ Building a Kubernetes Cluster.sh
@@ -138,6 +138,10 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
+# Install the Calico network add-on
+
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
+
 # Verify the cluster is working
 
 kubectl get nodes
