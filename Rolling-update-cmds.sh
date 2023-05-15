2addd558720532d6231728fe7811c1b62a8b640c
demo1289 <demostration1289@proto.me>
2023-05-16 07:36:29 +0800

    added blue green deployment cmds

--- Rolling-update-cmds.sh
+++ Rolling-update-cmds.sh
@@ -10,11 +10,26 @@ kubectl rollout status deployment/rolling-deployment
 # status of pods
 kubectl get pods
 
-# have a look at current deployment
+# have a look at current deployment to get the container image name
 kubectl describe deployment web-frontend -n hive
 
 # alias for kubectl
 alias k=kubectl
 
+# rolling up of image using set image
+kubectl set image deployment.v1.apps/web-frontend -n hive nginx=nginx:1.16.1
 
+# check hive namespace rollout status
+k rollout status deployment web-frontend -n hive
 
+# deploy green deployment
+ k apply -f internal-api-green.yml 
+
+# get ip address of pod
+k get pods -n hive -o wide
+
+# edit service
+k edit svc api-svc -n hive
+
+# get service
+k get svc api-svc -n hive
