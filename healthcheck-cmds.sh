e1255c02ce1273cd8db108fd7a603d57b96f9af5
demo1289 <demostration1289@proto.me>
2023-06-12 09:14:50 +0800

    updating healthcheck cmds

--- healthcheck-cmds.sh
+++ healthcheck-cmds.sh
@@ -1,1 +1,18 @@
 kubectl edit deployment comb-dashboard -n hive
+
+# adding livenessProbe
+    ```
+        livenessProbe:
+          httpGet:
+            path: /
+            port: 80
+          initialDelaySeconds: 3
+          periodSeconds: 3
+```
+
+# adding readinessProbe
+        readinessProbe:
+          exec:
+            command: ['echo', 'This is a test!']
+          initialDelaySeconds: 3
+          periodSeconds: 3
