cfc6accca9f56bca87cc6306a2344c554eb2ae1d
demo1289 <demostration1289@proto.me>
2023-06-26 07:21:36 +0800

    find out stuff

--- ConfigMap-Secret-cmd.sh
+++ ConfigMap-Secret-cmd.sh
@@ -1,2 +1,11 @@
 # ConfigMap and Secret
 echo secretToken! | base64
+
+# find out namespaces 
+k get deployments --all-namespaces
+
+# get secrets
+k get secrets -n hive
+
+# get configMap
+k get configMap -n hive
