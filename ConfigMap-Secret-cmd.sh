35cd380003aac39e019e123f1b2e9e88e9f33a44
demo1289 <demostration1289@proto.me>
2023-06-28 08:21:04 +0800

    Updated files

--- ConfigMap-Secret-cmd.sh
+++ ConfigMap-Secret-cmd.sh
@@ -9,3 +9,9 @@ k get secrets -n hive
 
 # get configMap
 k get configMap -n hive
+
+# get all deployment name and namespace
+k get deployment --all-namesapces
+
+# edit deployment
+k edit deployment hive-mgr -n hive
