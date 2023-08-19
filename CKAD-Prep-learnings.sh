4baf98b3a6442d2d8b95069f4f45d7b6ee8d618f
demo1289 <demostration1289@proto.me>
2023-08-19 17:36:33 +0800

    prep

--- CKAD-Prep-learnings.sh
+++ CKAD-Prep-learnings.sh
@@ -3,3 +3,6 @@ kubectl config use-context c1 --use-namespace n1
 
 # list resources with shortcuts, distinctly this refers to API resources which is one type of resource different from CPU or memory
 kubectl api-resources
+
+# piping to grep with 10 lines before and after search item
+kubectl explain --recursive |grep -c 10 readinessProbe
---
+++ t
@@ -0,0 +1,6 @@
+alias k=kubectl
+alias ke="kubectl explain --recursive"
+alias ka=kubectl apply -f"
+alias kd="kubectl describe"
+alias kc="kubectl config use-context"
+export do="--save-config --dry-run=client -o yaml"
