d1ffd8f6654b76df9f8ce91a5a0c5789703194b3
demo1289 <demostration1289@proto.me>
2023-08-22 09:28:47 +0800

    acloudguru exam prep labels

--- CKAD-Prep-learnings.sh
+++ CKAD-Prep-learnings.sh
@@ -6,3 +6,17 @@ kubectl api-resources
 
 # piping to grep with 10 lines before and after search item
 kubectl explain --recursive |grep -c 10 readinessProbe
+
+# practices for a cloud guru, exam prep
+12: Application Design and Build (lack )
+11: Application Environment, Configuration and Security (lack crd, )
+10: Application Deployment - blue/green, canary (lack helm, rolling updates)
+9: Services & Networking - nodePort, NetworkPolicy (lack ingress)
+8: Application observability and maintenance - liveness probe (lack debugging, container logs)
+7: Application Design and Build - cronjob, deployment (lack job)
+6: Application Environment, Configuration and Security - resource Request
+5: Application Design and Build - persistent volume
+4: Application Deployment - rolling update, roll back
+3: Application Environment, Configuration and Security - configMap
+2: Application Environment, Configuration and Security - service account, security settings 
+1: Application observability and maintenance - fix deployment, broken image
---
+++ DOCKER-LABS/build-app1/hello.py
@@ -0,0 +1,1 @@
+hello world, this is Nigel please to meet you! :-)
